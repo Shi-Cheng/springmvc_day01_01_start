fc68e32b43753d221eb27830a525c73c66638a50
Shi-Cheng <x_pan2016@163.com>
2019-03-04 22:14:20 +0800

    0.1

--- src/main/java/cn/dx/controller/paramController.java
+++ src/main/java/cn/dx/controller/paramController.java
@@ -4,17 +4,28 @@ import cn.dx.domain.Account;
 import cn.dx.domain.Person;
 import cn.dx.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/first")
+@SessionAttributes(value = {"username","password","age"},types = {String.class,Integer.class})
 public class paramController {
-
-    @Autowired
-    private static User user;
 
     @Autowired
     private static Account account;
@@ -52,4 +63,82 @@ public class paramController {
         System.out.println("测试成功");
         return new ModelAndView("redirect:/html/success.html");
     }
+
+    @RequestMapping("/testServelet")
+    public String testServelet(HttpServletRequest request,HttpServletResponse response){
+        System.out.println("执行了....");
+        System.out.println(request);
+
+        HttpSession session = request.getSession();
+        ServletContext servletContext = session.getServletContext();
+        System.out.println(servletContext);
+
+        System.out.println(response);
+        System.out.println();
+        return "html/success";
+    }
+
+    /**
+     * modelAttribute 有参返回类型
+     * @return
+     */
+    @ModelAttribute
+    public User showUser(){
+        System.out.println("showUser 方法执行了...");
+        User user = new User();
+        user.setUname("王小六");
+        user.setAge(13);
+        return user;
+    }
+    /**
+     * 输出user
+     * @param user
+     * @return
+     */
+    @RequestMapping("/updateUser")
+    public String updateUser(User user){
+        System.out.println(user);
+        return "html/success";
+    }
+
+
+    @ModelAttribute
+    public void showVoidUser(String name, Map<String,User> map){
+        User user = new User();
+        user.setUname("小琪");
+        user.setAge(12);
+        map.put("abc",user);
+    }
+    /**
+     * modelAttribute 无返回传值  通过键值对的方式获取对应的字段
+     * @param user
+     * @return
+     */
+    @RequestMapping("/updateVoidUser")
+    public String updateVoidUser(@ModelAttribute(value = "abc") User user){
+        System.out.println(user);
+        return "html/success";
+    }
+
+    @RequestMapping("/saveSessionAttribute")
+    public String SaveSessionAttribute(Model model){
+        System.out.println("向session中存入值");
+        model.addAttribute("username","张三");
+        model.addAttribute("password","12345");
+        model.addAttribute("age",20);
+
+        return "html/success";
+    }
+
+    @RequestMapping("/getSessionAttribute")
+    public String getSessionAttribute(ModelMap modelMap){
+        System.out.println("ModelMap 方法执行了...");
+        String username = (String) modelMap.get("username");
+        String password = (String) modelMap.get("password");
+        Integer age = (Integer) modelMap.get("age");
+
+        System.out.println("============"+username+", "+password + ", "+ age);
+        return "html/success";
+    }
+
 }
